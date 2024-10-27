51268e5611b51a5ce88881023404aaf21d9c83bd
Gabriel Pettinari <aalegabpett04@gmail.com>
2024-10-27 10:45:05 -0300

    Actualización de ejercicio

--- importando_funciones/funtion.py
+++ importando_funciones/funtion.py
@@ -1,22 +1,37 @@
 
 ## Generación de la clase con sus funciones
 class Person():
-  def __init__(self, name, last_name, age):
+  def __init__(self, name, last_name, age, height, profession):
     self.name = name
     self.last_name = last_name
     self.age = age
+    self.height = height
+    self.profession = profession
   
   ## En esta función se pasa la estructura del print solicitado y con los datos pasados desde 'import_funtion.py'
   def __str__(self):
-    return "nombre: {}, apellido: {}, edad: {}".format(self.name, self.last_name, self.age)
+    espacio = '-'*22
+    print = (f'''
+            {espacio}
+             nombre   : {self.name}, 
+             apellido : {self.last_name}, 
+             edad     : {self.age}, 
+             altura   : {self.height}, 
+             profesión: {self.profession}
+    ''')
+    return print
 
-    '''
-    TAMBIÉN SE PUEDE UTILIZAR EN 'def __str__(self):' de la siguiente forma...
-    def __str__(self):
-      return super().__str__() + "ejemplo de texto {}, otro ejemplo de texto {}
-      etc, etc".format(self.ejemplo, self.ejemplo......)
-      
-    '''
+'''
+TAMBIÉN SE PUEDE UTILIZAR EN 'def __str__(self):' de la siguiente forma...
+def __str__(self):
+  return super().__str__() + "Aca va texto {}, otro texto {}
+  etc, etc".format(self.ejemplo, self.ejemplo......)
+  
+QUEDARÍA:
+return super().__str__() + "nombre: {}, apellido: {}, edad: {}, altura: {}, profesión: {}".format(self.name, self.last_name, self.age, self.height, self.profession)
 
+
+return "nombre: {}, apellido: {}, edad: {}, altura: {}, profesión: {}".format(self.name, self.last_name, self.age, self.height, self.profession)
+'''
 
 
--- importando_funciones/import_funtion.py
+++ importando_funciones/import_funtion.py
@@ -2,8 +2,11 @@
 from funtion import Person
 
 ## Se llama a la clase y se le pasan los datos que solicita la función para poder imprimir los datos.
-print(Person('Gabriel', 'Peralta', 35))
 
+first_person = Person('Gabriel', 'Peralta', 35, 1.75, 'Contador')
+print(first_person)
 
+second_person = Person('Marcos', 'Pérez', 22, 1.81, 'Abogado')
+print(second_person)
 
 
